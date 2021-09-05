351c84dcae88ce1aa11c9b9db5689fa172b44d18
Fahrul Arya Sugita <faryasugita@gmail.com>
2021-09-06 00:44:41 +0700

    istimiwir

---
+++ src/test/java/com/fahrul/springwebfluxvideostreaming/SpringWebfluxVideostreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahrul.springwebfluxvideostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxVideostreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
